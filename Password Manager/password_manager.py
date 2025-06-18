ad2894990ad49748ccffbf8c89f428e27f1979da
Musawer Afzal <musawerafzal23@gmail.com>
2025-06-18 17:14:11 +0500

    I Kinda Messed up

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -1,13 +1,35 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
 master__pwd = input("Please enter your master password: ")
+key = load_key() + master__pwd.encode()
+fer = Fernet(key)
+
 
 def view():
-    pass
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, pwd = data.split("|")
+        print("User:", user, "| Password:", fer.decrypt(pwd.encode()).decode())
+
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("passwords.txt", 'a') as f:
-        f.write(f"{name} | {pwd}\n")
+        f.write(f"{name} | {fer.encrypt(pwd.encode()).decode()} \n")
 
 while True:
     mode = input("Would you like to add or view passwords? (add/view). Type q to quit ").lower()
